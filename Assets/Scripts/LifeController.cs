2f58bbb8be8ebba2b3bf3367935a22dcefd86f19
LeoLinh <nnhatlinhh@gmail.com>
2024-02-17 23:00:56 +0700

    update
    update

--- Assets/Scripts/LifeController.cs
+++ Assets/Scripts/LifeController.cs
@@ -23,10 +23,7 @@ public class LifeController : MonoBehaviour
     {
         thePlayer = FindFirstObjectByType<PlayerController>();
 
-        if (UIController.instance != null)
-        {
-            UIController.instance.UpdateLivesDisplay(currentLives);
-        }
+        UpdateDisplay();
     }
 
     // Update is called once per frame
@@ -61,6 +58,8 @@ public class LifeController : MonoBehaviour
             UIController.instance.UpdateLivesDisplay(currentLives);
         }
 
+        UpdateDisplay();
+
         Instantiate(deathEffect, thePlayer.transform.position, deathEffect.transform.rotation);
     }
 
@@ -84,6 +83,21 @@ public class LifeController : MonoBehaviour
         if (UIController.instance != null)
         {
             UIController.instance.ShowGameOver();
+        }
+    }
+
+    public void AddLife()
+    {
+        currentLives++;
+
+        UpdateDisplay();
+    }
+
+    public void UpdateDisplay()
+    {
+        if (UIController.instance != null)
+        {
+            UIController.instance.UpdateLivesDisplay(currentLives);
         }
     }
 }
